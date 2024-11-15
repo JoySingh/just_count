2cdff8f4f77664b7263d6952c1336f4576fe52c5
JoySingh <joyprietsingh@gmail.com>
2024-11-15 15:54:01 +0100

    added argument to choose between square and square root

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,12 +1,19 @@
 import click
 
 import just_count.square
+import just_count.square_root
 
 
 @click.command()
 @click.argument("number", type=int)
-def main(number):
-    print(f"The square of {number} is {just_count.square.square(number)}")
+@click.argument("sqrt_or_sqr")
+def main(number, sqrt_or_sqr):
+    if sqrt_or_sqr == "square":
+        print(f"The square of {number} is {just_count.square.square(number)}")
+    elif sqrt_or_sqr == "square_root":
+        print(
+            f"The square root of {number} is {just_count.square_root.square_root(number)}"
+        )
 
 
 if __name__ == "__main__":
---
+++ src/just_count/square_root.py
@@ -0,0 +1,6 @@
+def square_root(x):
+    return x ** (1 / 2)
+
+
+if __name__ == "__main__":
+    print(f"The square of 4 is {square_root(4)}")
